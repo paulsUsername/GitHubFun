1a6cda2b132a09bf1bdb6b4186c99b36397c9086
paulsUsername <32492218+paulsUsername@users.noreply.github.com>
2017-10-09 13:32:36 +0100

    Initial Commit

---
+++ GitHubFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubFun
+//
+//  Created by Paul Finlay on 09/10/2017.
+//  Copyright © 2017 Paul Finlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
