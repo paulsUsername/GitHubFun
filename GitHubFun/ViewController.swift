5b31924a1bf97c3a62d6cbc456392285e6756baf
paulsUsername <32492218+paulsUsername@users.noreply.github.com>
2017-10-09 13:39:58 +0100

    I've changed the background to yellow.

--- GitHubFun/ViewController.swift
+++ GitHubFun/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.yellow
         
         
     }
